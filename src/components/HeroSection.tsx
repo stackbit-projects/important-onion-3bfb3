d2e6a700db7065ce2b1322b179f99fd91dbd834b
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 12:49:56 +0000

    HeroSection.tsx: changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 import { getComponent } from '@stackbit/components/dist/components-registry';
-import { mapStylesToClassNames as mapStyles } from '../../utils/map-styles-to-class-names';
+import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names';
 
 export default function HeroSection(props) {
     const colors = props.colors || 'colors-a';
