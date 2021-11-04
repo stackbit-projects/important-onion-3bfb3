75da0c5eaa6070f4f4e91cda24cfcb8ab9d2b06c
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 12:57:08 +0000

    Callout.js: changed

--- src/components/Callout.js
+++ src/components/Callout.js
@@ -1,7 +1,12 @@
 // src/components/Callout.js
+// src/components/Callout.js
 
-const Callout = () => {
-  return <p>Hello! I am a callout.</p>
+const Callout = ({ body }) => {
+  return (
+    <div>
+      <p>{body}</p>
+    </div>
+  )
 }
 
 export default Callout
